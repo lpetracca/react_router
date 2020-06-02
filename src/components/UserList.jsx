46d5bf4d42562659f400fb093a443fb97f5f047c
lpetracca <lucia.petracca@yahoo.com>
2020-06-02 09:35:20 -0300

    10 users

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import jsonplaceholder from '../api/jsonplaceholder';
 
 const UserList = () => {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        jsonplaceholder.get('/users').then(response => console.log(response.data));
+        jsonplaceholder
+            .get('/users')
+            .then(response => setUsers(response.data));
     }, []);
 
-    return <div> UserList </div>
+    const userList = users.map(item => {
+        return (
+        <li key={item.id}>
+            <Link to={`/user/${item.id}`} >
+            {item.name}
+            </Link>
+        </li>)
+    });
+
+    return (<div>
+        <ul> {userList}</ul>
+    </div>);
 
 };
 
